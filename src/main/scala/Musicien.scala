bc46db87fdf8fa283a906867fde5b9927de7c1c0
kismath.adeleke <kismath.adeleke@habiteo.com>
2024-03-14 12:29:10 +0100

    add some case class

--- src/main/scala/Musicien.scala
+++ src/main/scala/Musicien.scala
@@ -3,17 +3,63 @@ package upmc.akka.leader
 import akka.actor._
 
 case class Start ()
+case class Bandmaster(id : Int)
+case class ElectBandmaster()
+case class IsElected()
+case class Leave()
+
 
 class Musicien (val id:Int, val terminaux:List[Terminal]) extends Actor {
 
-     // Les differents acteurs du systeme
-     val displayActor = context.actorOf(Props[DisplayActor], name = "displayActor")
-     val database = context.actorOf(Props[DataBaseActor], name = "database")
-     def receive = {
-          case Start => {
-               displayActor ! Message ("Musicien " + this.id + " is created")
-               
-          }
+    // Les differents acteurs du systeme
+    val displayActor = context.actorOf(Props[DisplayActor], name = "displayActor")
+    val database = context.actorOf(Props[DataBaseActor], name = "database")
+    val player = context.actorOf(Props[PlayerActor], name = "player")
 
-     }
+    var availableMusicians = new Array[Boolean](4)
+    var allMusicians = new Array[ActorSelection](4)
+    var leader = -1
+
+    availableMusicians(id) = true 
+
+    for(i <- 0 to 3) {
+        if (i != id){
+            allMusicians(i) = context.actorSelection("akka.tcp://MozartSystem" + i + "@127.0.0.1:600" + i + "/user/Node" + i + "/Musicien" + i)
+            availableMusicians(i) = false
+        }
+    }
+
+    def receive = {
+        case Start => {
+            displayActor ! Message ("Musicien " + this.id + " is created")
+        }
+        case Bandmaster(idMusician) => {
+            if (idMusician != this.id && availableMusicians(idMusician)){
+                leader = idMusician
+            }
+        }
+
+        case ElectBandmaster() => {
+            var someOneElected = false
+            var i = 0
+            while (i < 4 && !someOneElected) {
+                if (availableMusicians(i)) {
+                    someOneElected = true
+                    if (i == id) 
+                        self ! IsElected
+                    else 
+                        leader = i
+                }
+                i += 1
+            }
+        }
+
+        case IsElected =>
+            if(leader != id) {
+                leader = id
+                displayActor ! Message ("Musicien " + this.id + " est le leader")
+                self ! player
+            }
+        
+    }
 }
--- src/main/scala/Projet.scala
+++ src/main/scala/Projet.scala
@@ -7,38 +7,38 @@ case class Terminal (id:Int, ip:String, port:Int)
 
 object Projet {
 
-   
-     def main (args : Array[String]) {
-          // Gestion des erreurs
-          if (args.size != 1) {
-               println ("Erreur de syntaxe : run <num>")
-               sys.exit(1)
-          }
 
-          val id : Int = args(0).toInt
+    def main (args : Array[String]) {
+        // Gestion des erreurs
+        if (args.size != 1) {
+            println ("Erreur de syntaxe : run <num>")
+            sys.exit(1)
+        }
 
-          if (id < 0 || id > 3) {
-               println ("Erreur : <num> doit etre compris entre 0 et 3")
-               sys.exit(1)
-          }
+        val id : Int = args(0).toInt
 
-          var musicienlist = List[Terminal]()
-          
-          // recuperation des adresses de tous les musiciens
-          // hardcoded path name
-          for(i <- 3 to 0 by -1){
-               val address = ConfigFactory.load().getConfig("system"+ i).getValue("akka.remote.netty.tcp.hostname").render()
-               val port = ConfigFactory.load().getConfig("system"+ i).getValue("akka.remote.netty.tcp.port").render()
-               musicienlist = Terminal(i, address, port.toInt)::musicienlist
-          }
+        if (id < 0 || id > 3) {
+            println ("Erreur : <num> doit etre compris entre 0 et 3")
+            sys.exit(1)
+        }
 
-          println(musicienlist)
+        var musicienlist = List[Terminal]()
+        
+        // recuperation des adresses de tous les musiciens
+        // hardcoded path name
+        for(i <- 3 to 0 by -1){
+            val address = ConfigFactory.load().getConfig("system"+ i).getValue("akka.remote.netty.tcp.hostname").render()
+            val port = ConfigFactory.load().getConfig("system"+ i).getValue("akka.remote.netty.tcp.port").render()
+            musicienlist = Terminal(i, address, port.toInt)::musicienlist
+        }
 
-          // Initialisation du node <id>
-          val system = ActorSystem("MozartSystem" + id, ConfigFactory.load().getConfig("system" + id))
-          val musicien = system.actorOf(Props(new Musicien(id, musicienlist)), "Musicien"+id)
+        println(musicienlist)
 
-          musicien ! Start
-     }
+        // Initialisation du node <id>
+        val system = ActorSystem("MozartSystem" + id, ConfigFactory.load().getConfig("system" + id))
+        val musicien = system.actorOf(Props(new Musicien(id, musicienlist)), "Musicien"+id)
+
+        musicien ! Start
+    }
 
 }
